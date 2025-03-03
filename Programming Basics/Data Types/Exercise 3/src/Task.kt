f7f8497437bee2b1af1db4d8b389d357933c0fea
chimaek <pipiru100@gmail.com>
2025-03-03 18:44:34 +0900

    init

--- Programming Basics/Data Types/Exercise 3/src/Task.kt
+++ Programming Basics/Data Types/Exercise 3/src/Task.kt
@@ -2,10 +2,10 @@
 package dataTypesExercise3
 
 // 'a' + 1
-val c1 = '?'
+val c1: Char = 'a' + 1;
 
 // 'a' + 25
-val c2 = '?'
+val c2: Char = 'a' + 25;
 
 // 'E' - 2
-val c3 = '?'
+val c3: Char = 'E' - 2
